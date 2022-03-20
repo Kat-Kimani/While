ade34e071b35b544144f31ed839fdd4f48a25113
Kat - Kimani <93208116+Kat-Kimani@users.noreply.github.com>
2022-03-20 14:26:08 +0000

    Update FoodRequestor.java
    Corrected class

--- FoodRequestor.java
+++ FoodRequestor.java
@@ -18,17 +18,4 @@ public class FoodRequestor {
 		newFood.enterFood(food,cont);
 			
 	}
-		public static void enterFoodDoWhile() {
-
-		String food;
-		String cont;
-		Scanner scan = new Scanner(System.in);
-		do {
-			System.out.println("Please enter a food you like");
-			food = scan.next();
-			System.out.println("Would you like to continue?");
-			cont = scan.next();
-		} while (!cont.equals("N"));
-		
-	}
 }
